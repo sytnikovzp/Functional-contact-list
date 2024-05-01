1bcd24b1a2227ccbdaf4453fb495c07a457c4fa3
alex <sytnikov.zp@gmail.com>
2024-05-01 10:32:45 +0300

    Filled out actionTypes

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,12 @@
+const ACTION_TYPES = {
+  GET_CONTACTS: 'getContacts',
+  CREATE_EMPTY_CONTACT: 'createEmptyContact',
+  SELECT_CONTACT: 'selectContact',
+  ADD_NEW_CONTACT: 'addNewContact',
+  CREATE_CONTACT: 'createContact',
+  UPDATE_CONTACT: 'updateContact',
+  SAVE_CONTACT: 'saveContact',
+  DELETE_CONTACT: 'deleteContact',
+};
+
+export default ACTION_TYPES;
