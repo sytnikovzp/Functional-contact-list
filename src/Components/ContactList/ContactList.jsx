2ccc2f379ac3e1f3f5f08e37355d0d8449fea3b1
alex <sytnikov.zp@gmail.com>
2024-05-01 20:03:55 +0300

    Refactoring ContactList.jsx with Redux

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -1,24 +1,32 @@
 import ContactItem from './ContactItem/ContactItem';
-import PropTypes from 'prop-types';
+import { getContacts } from '../../store/actions/contactActions';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import api from '../../api/contact-service';
 import './ContactList.css';
 
-function ContactList({ contacts, onNewContact, onEditContact, onDelete }) {
+function ContactList() {
+  const dispatch = useDispatch();
+
+  const contacts = useSelector((state) => state.arrContacts);
+
+  useEffect(() => {
+    api.get('/contacts').then(({ data }) => dispatch(getContacts(data)));
+  }, [dispatch]);
+
   return (
     <div id='wrapper-list'>
       <div id='contact-list'>
         {contacts.map((contact) => {
-          return (
-            <ContactItem
-              key={contact.id}
-              contact={contact}
-              onEdit={onEditContact}
-              onDelete={onDelete}
-            />
-          );
+          return <ContactItem key={contact.id} contact={contact} />;
         })}
       </div>
       <div className='btn-list-block'>
-        <button className='btn' id='new-btn' onClick={onNewContact}>
+        <button
+          className='btn'
+          id='new-btn'
+          // onClick={onNewContact}
+        >
           New
         </button>
       </div>
@@ -26,15 +34,15 @@ function ContactList({ contacts, onNewContact, onEditContact, onDelete }) {
   );
 }
 
-ContactList.propTypes = {
-  contacts: PropTypes.array,
-  onNewContact: PropTypes.func,
-  onEditContact: PropTypes.func,
-  onDelete: PropTypes.func,
-};
+// ContactList.propTypes = {
+//   contacts: PropTypes.array,
+//   onNewContact: PropTypes.func,
+//   onEditContact: PropTypes.func,
+//   onDelete: PropTypes.func,
+// };
 
-ContactList.defaultProps = {
-  contacts: [],
-};
+// ContactList.defaultProps = {
+//   contacts: [],
+// };
 
 export default ContactList;
