82431d4872b629c69bdf3148e7593400fcd77f79
alex <sytnikov.zp@gmail.com>
2024-05-05 14:47:07 +0300

    Add redux-saga to project

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import reducer from './reducers/contactsReducer';
+import createSagaMiddleware from 'redux-saga';
 
-export default createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(logger))
-);
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = applyMiddleware(sagaMiddleware, logger);
+
+export default createStore(reducer, composeWithDevTools(middleware));
+
+sagaMiddleware.run({});
