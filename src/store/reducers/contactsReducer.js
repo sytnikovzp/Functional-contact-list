18c043fbcffc477ff4d01496bfe54b1a78d9abf8
alex <sytnikov.zp@gmail.com>
2024-05-05 16:14:45 +0300

    Refactoring contactsReducer by Redux-Saga

--- src/store/reducers/contactsReducer.js
+++ src/store/reducers/contactsReducer.js
@@ -4,6 +4,8 @@ import ACTION_TYPES from '../actions/actionTypes';
 const initialState = {
   arrContacts: contactsState,
   currentContact: createEmptyContact(),
+  isPending: false,
+  error: null,
 };
 
 export default function contactsReducer(
@@ -11,12 +13,65 @@ export default function contactsReducer(
   { type, payload }
 ) {
   switch (type) {
-    case ACTION_TYPES.GET_CONTACTS:
+    // Success
+    case ACTION_TYPES.POST_CONTACT_SUCCESS:
+      return {
+        ...state,
+        arrContacts: [...state.arrContacts, payload],
+        currentContact: createEmptyContact(),
+        isPending: false,
+      };
+
+    case ACTION_TYPES.PUT_CONTACT_SUCCESS:
+      return {
+        ...state,
+        arrContacts: [
+          ...state.arrContacts.map((contact) =>
+            contact.id !== payload.id ? contact : payload
+          ),
+        ],
+        isPending: false,
+      };
+
+    case ACTION_TYPES.DELETE_CONTACT_SUCCESS:
+      return {
+        ...state,
+        arrContacts: [
+          ...state.arrContacts.filter((contact) => contact.id !== payload),
+        ],
+        currentContact: createEmptyContact(),
+        isPending: false,
+      };
+
+    case ACTION_TYPES.GET_CONTACTS_SUCCESS:
       return {
         ...state,
         arrContacts: payload,
+        isPending: false,
       };
 
+    // Requesting
+    case ACTION_TYPES.POST_CONTACT_REQUEST:
+    case ACTION_TYPES.PUT_CONTACT_REQUEST:
+    case ACTION_TYPES.DELETE_CONTACT_REQUEST:
+    case ACTION_TYPES.GET_CONTACTS_REQUEST:
+      return {
+        ...state,
+        isPending: true,
+      };
+
+    // Error
+    case ACTION_TYPES.POST_CONTACT_ERROR:
+    case ACTION_TYPES.PUT_CONTACT_ERROR:
+    case ACTION_TYPES.DELETE_CONTACT_ERROR:
+    case ACTION_TYPES.GET_CONTACTS_ERROR:
+      return {
+        ...state,
+        isPending: false,
+        error: payload,
+      };
+
+    // Other
     case ACTION_TYPES.SELECT_CONTACT:
       return {
         ...state,
@@ -26,32 +81,6 @@ export default function contactsReducer(
     case ACTION_TYPES.ADD_NEW_CONTACT:
       return {
         ...state,
-        currentContact: createEmptyContact(),
-      };
-
-    case ACTION_TYPES.CREATE_CONTACT:
-      return {
-        ...state,
-        arrContacts: [...state.arrContacts, payload],
-        currentContact: createEmptyContact(),
-      };
-
-    case ACTION_TYPES.UPDATE_CONTACT:
-      return {
-        ...state,
-        arrContacts: [
-          ...state.arrContacts.map((contact) =>
-            contact.id !== payload.id ? contact : payload
-          ),
-        ],
-      };
-
-    case ACTION_TYPES.DELETE_CONTACT:
-      return {
-        ...state,
-        arrContacts: [
-          ...state.arrContacts.filter((contact) => contact.id !== payload),
-        ],
         currentContact: createEmptyContact(),
       };
 
