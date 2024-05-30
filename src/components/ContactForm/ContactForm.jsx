d170b4e0e7eb08ee547d5ee3f31d7494fc18372f
alex <sytnikov.zp@gmail.com>
2024-05-31 00:40:36 +0300

    The function itself is passed inside the Formik component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -171,9 +171,7 @@ function ContactForm() {
       validationSchema={schema}
       enableReinitialize
     >
-      {({ errors, touched, setFieldValue }) =>
-        renderForm({ errors, touched, setFieldValue })
-      }
+      {renderForm}
     </Formik>
   );
 }
